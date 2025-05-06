926cf0dec23ff6bd98da504a5054e64e2e446a99
Nicolas Ppelis <nicolas_ppelis@cybeem.com>
2025-05-06 09:58:38 +0300

    060520251

--- woocommerce_fusion/public/js/stock/item.js
+++ woocommerce_fusion/public/js/stock/item.js
@@ -1,51 +1,74 @@
+function is_wc_enabled_wrapper(fnCallback)
+{
+
+	frappe.db.get_single_value("CP Settings", "woocommerce_sync").then((value) => {
+		if(value) {
+			fnCallback();
+		}
+	});	
+}
+
 frappe.ui.form.on('Item', {
 	onload_post_render: function(frm) {
 
 	},
 	refresh: function(frm) {
-		// Add a custom button to sync Item Stock with WooCommerce
-		frm.add_custom_button(__("Sync this Item's Stock Levels to WooCommerce"), function () {
-			frm.trigger("sync_item_stock");
-		}, __('Actions'));
 
-		// Add a custom button to sync Item Price with WooCommerce
-		frm.add_custom_button(__("Sync this Item's Price to WooCommerce"), function () {
-			frm.trigger("sync_item_price");
-		}, __('Actions'));
+		is_wc_enabled_wrapper(() => {
+				// Add a custom button to sync Item Stock with WooCommerce
+				frm.add_custom_button(__("Sync this Item's Stock Levels to WooCommerce"), function () {
+					frm.trigger("sync_item_stock");
+				}, __('Actions'));
 
-		// Add a custom button to sync Item with WooCommerce
-		frm.add_custom_button(__("Sync this Item with WooCommerce"), function () {
-			frm.trigger("sync_item");
-		}, __('Actions'));
+				// Add a custom button to sync Item Price with WooCommerce
+				frm.add_custom_button(__("Sync this Item's Price to WooCommerce"), function () {
+					frm.trigger("sync_item_price");
+				}, __('Actions'));
+
+				// Add a custom button to sync Item with WooCommerce
+				frm.add_custom_button(__("Sync this Item with WooCommerce"), function () {
+					frm.trigger("sync_item");
+				}, __('Actions'));
+		})
 	},
 
 	sync_item_stock: function(frm) {
 		// Sync this Item
 
-		console.log('sync ....')
 
-		frappe.dom.freeze(__("Sync Item Stock with WooCommerce..."));
-		frappe.call({
-			method: "woocommerce_fusion.tasks.stock_update.update_stock_levels_on_woocommerce_site",
-			args: {
-				item_code: frm.doc.name
-			},
-			callback: function(r) {
-				frappe.dom.unfreeze();
-				frappe.show_alert({
-					message:__('Synchronised stock level to WooCommerce for enabled servers'),
-					indicator:'green'
-				}, 5);
-				frm.reload_doc();
-			},
-			error: (r) => {
-				frappe.dom.unfreeze();
-				frappe.show_alert({
-					message: __('There was an error processing the request. See Error Log.'),
-					indicator: 'red'
-				}, 5);
-			}
-		});
+
+
+
+		is_wc_enabled_wrapper(		() => {
+			console.log('sync ....')
+
+			frappe.dom.freeze(__("Sync Item Stock with WooCommerce..."));
+			frappe.call({
+				method: "woocommerce_fusion.tasks.stock_update.update_stock_levels_on_woocommerce_site",
+				args: {
+					item_code: frm.doc.name
+				},
+				callback: function(r) {
+					frappe.dom.unfreeze();
+					frappe.show_alert({
+						message:__('Synchronised stock level to WooCommerce for enabled servers'),
+						indicator:'green'
+					}, 5);
+					frm.reload_doc();
+				},
+				error: (r) => {
+					frappe.dom.unfreeze();
+					frappe.show_alert({
+						message: __('There was an error processing the request. See Error Log.'),
+						indicator: 'red'
+					}, 5);
+				}
+			});
+		})
+
+
+
+
 	},
 
 	sync_item_price: function(frm) {
